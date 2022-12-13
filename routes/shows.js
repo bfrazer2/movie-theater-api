040cfd9610beb0e1e4c321159ab0a6cd6f55fa59
Benjamin Frazer <benjamin.frazer@benjamin.frazer-MacBook-Pro>
2022-12-13 17:34:44 -0500

    added server side validation for entries into rating and status

--- routes/shows.js
+++ routes/shows.js
@@ -6,6 +6,10 @@ const router  = express.Router({mergeParams: true})
 //requiring in Show from index.js
 const { Show } = require('../models/index')
 
+//requiring in express-validator
+const {check, validationResult} = require("express-validator")
+
+
 //Including middleware so users can enter data into the body of the request
 router.use(express.json())
 router.use(express.urlencoded({extended: true}))
@@ -29,20 +33,32 @@ router.get("/genre/:genre", async (req,res) => {
     res.send(showsByGenre)
 })
 
-router.put("/:showId/watched", async (req,res) => {
-    const showByID = await Show.findByPk(req.params.showId)
-    await showByID.update({
+router.put("/:showId/watched", [check("rating").trim().not().isEmpty()], async (req,res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        res.json({error: errors.array()})
+    } else {
+        const showByID = await Show.findByPk(req.params.showId)
+        await showByID.update({
         rating: req.body.rating
-    })
-    res.send(showByID)
+        })
+        res.send(showByID)
+    }
 })
 
-router.put("/:showId/updates", async (req,res) => {
-    const showByID = await Show.findByPk(req.params.showId)
-    await showByID.update({
+router.put("/:showId/updates", [check("status").trim().isLength({min: 5, max: 25})], async (req,res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        res.json({error: errors.array()})
+    } else {
+        const showByID = await Show.findByPk(req.params.showId)
+        await showByID.update({
         status: req.body.status
-    })
-    res.send(showByID)
+        })
+        res.send(showByID)
+    }
+    
+    
 })
 
 router.delete("/:showId", async (req,res) => {
